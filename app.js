78af43dbe71fd16388c664515acca5e8c15678f7
David Rodriguez <david@stakeordie.com>
2024-04-29 16:41:00 -0700

    fix: Add tokens json

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const tokens = require("./tokens.json")
 const port = process.env.PORT || 3001;
 
 app.get("/health", (req, res) => res.send("Ok"));
+
+app.get("/tokens/:id",async (req,res)=>{
+  console.log("tokens",tokens)
+  const data = tokens[req.params.id];
+  return res.json(data).send();
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
